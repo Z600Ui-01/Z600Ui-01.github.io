a93cce464955c36eca652ad163b32a5cb7425e7f
Z600Ui-01 <128437381+Z600Ui-01@users.noreply.github.com>
2024-06-23 21:38:48 +0900

    Add files via upload

--- leeyerim/bowers/js/main.js
+++ leeyerim/bowers/js/main.js
@@ -3,7 +3,51 @@ window.onload = function () {
     scrollTo(0, 0);
   }, 100);
 };
+/* --------------------------------------------------------- */
 
+/* GNB 호버반응 */
+
+$("#gnb .gnb01")
+.mouseenter(function () {
+  $("#gnb .select01").addClass("on");
+})
+.mouseleave(function () {
+  $("#gnb .select01").removeClass("on");
+});
+
+$("#gnb .gnb02")
+.mouseenter(function () {
+  $("#gnb .select02").addClass("on");
+})
+.mouseleave(function () {
+  $("#gnb .select02").removeClass("on");
+});
+
+$("#gnb .gnb03")
+.mouseenter(function () {
+  $("#gnb .select03").addClass("on");
+})
+.mouseleave(function () {
+  $("#gnb .select03").removeClass("on");
+});
+
+$("#gnb .gnb04")
+.mouseenter(function () {
+  $("#gnb .select04").addClass("on");
+})
+.mouseleave(function () {
+  $("#gnb .select04").removeClass("on");
+});
+
+$("#gnb .arrow")
+.mouseenter(function () {
+  $("#gnb .arrow02").addClass("on");
+})
+.mouseleave(function () {
+  $("#gnb .arrow02").removeClass("on");
+});
+
+/* --------------------------------------------------------- */
 /* --------------------------------------------------------- */
 
 setTimeout(() => {
